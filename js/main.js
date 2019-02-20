ad4c5b7fd94b7031b8fcd51bdaf5d604882385d6
evanryk <evanryk@gmail.com>
2019-02-20 17:15:49 +0200

    added box shadow to header button animation and created easing variables

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const nav = document.getElementById('nav');
 
 window.addEventListener("scroll", function(){
   var offset = window.pageYOffset; 
-  if(offset > 50) {
+  if(offset > 40) {
     nav.classList.add('scrolled');
   } else {
     nav.classList.remove('scrolled');
