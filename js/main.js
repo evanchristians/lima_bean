6f7f457f7ec1184f2d0c29ca92ce9ee7ca939183
evanryk <evanryk@gmail.com>
2019-02-21 12:45:20 +0200

    added header parallax and changed base font size

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const nav = document.getElementById('nav');
+const header = document.getElementById('header');
 
 window.addEventListener("scroll", function(){
   var offset = window.pageYOffset; 
+
+  // ANIMATION NAVBAR WHEN SCROLLED DOWN
   if(offset > 40) {
     nav.classList.add('scrolled');
   } else {
     nav.classList.remove('scrolled');
   }
+
+  // HEADER BACKGROUND PARALLAX
+
+  header.style.backgroundPositionY = (offset * .4) + "px";
 })
