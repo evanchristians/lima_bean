ea2b0e152bcee8b972013e66a3c22663b15ca2cf
evanryk <evanryk@gmail.com>
2019-02-21 16:02:42 +0200

    added hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 const nav = document.getElementById('nav');
 const header = document.getElementById('header');
+const burgBtn = document.getElementById('burgerBtn');
+const burgBtnAlt = document.getElementById('burgerBtnAlt');
+
+burgBtn.addEventListener("click", function(){
+  document.getElementById('burger_items').classList.add("shown")
+  burgBtn.style.display = "none";
+  burgBtnAlt.style.display = "block";
+})
+burgBtnAlt.addEventListener("click", function(){
+  document.getElementById('burger_items').classList.remove("shown")
+  burgBtnAlt.style.display = "none";
+  burgBtn.style.display = "block";
+})
 
 window.addEventListener("scroll", function(){
   var offset = window.pageYOffset; 
