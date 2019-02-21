5fdf8c8cba1c76baad5eee0f94ddd897ca2817ae
evanryk <evanryk@gmail.com>
2019-02-21 20:35:48 +0200

    added hamburger menu icon

--- js/main.js
+++ js/main.js
@@ -4,14 +4,32 @@ const burgBtn = document.getElementById('burgerBtn');
 const burgBtnAlt = document.getElementById('burgerBtnAlt');
 
 burgBtn.addEventListener("click", function(){
-  document.getElementById('burger_items').classList.add("shown")
-  burgBtn.style.display = "none";
-  burgBtnAlt.style.display = "block";
+  document.getElementById('burger_items').style.display = "block";
+  setTimeout(function() {
+    document.getElementById('burger_items').classList.add("shown");
+  }, 5)
+  burgBtn.style.opacity = "0";
+  burgBtnAlt.style.display = "flex";
+  burgBtnAlt.style.opacity = "1";
+  burgBtnAlt.style.zIndex = "-1";
+  setTimeout(function(){
+    burgBtn.style.display = "none";
+    burgBtnAlt.style.zIndex = "1";
+  }, 300);
 })
 burgBtnAlt.addEventListener("click", function(){
   document.getElementById('burger_items').classList.remove("shown")
-  burgBtnAlt.style.display = "none";
-  burgBtn.style.display = "block";
+  burgBtnAlt.style.opacity = "0";
+  burgBtn.style.display = "flex";
+  burgBtn.style.opacity = "1";
+  burgBtn.style.zIndex = "-1";
+  setTimeout(function(){
+    burgBtnAlt.style.display = "none";
+    burgBtn.style.zIndex = "1";
+  }, 300);
+  setTimeout(function() {
+    document.getElementById('burger_items').style.display = "none";
+  }, 150);
 })
 
 window.addEventListener("scroll", function(){
